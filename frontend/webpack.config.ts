e0cb391492e76c2ce99dcf6d8611515a1fd70bb3
Aden Chua <37574101+adenchua@users.noreply.github.com>
2023-11-23 17:17:47 +0800

    add eslint rules

--- frontend/webpack.config.ts
+++ frontend/webpack.config.ts
@@ -1,34 +1,34 @@
-import path from 'path';
-import { Configuration as WebpackConfiguration } from 'webpack';
-import { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from "path";
+import { Configuration as WebpackConfiguration } from "webpack";
+import { Configuration as WebpackDevServerConfiguration } from "webpack-dev-server";
+import HtmlWebpackPlugin from "html-webpack-plugin";
 
 interface Configuration extends WebpackConfiguration {
   devServer?: WebpackDevServerConfiguration;
 }
 
 const config: Configuration = {
-  mode: (process.env.NODE_ENV as 'production' | 'development' | undefined) ?? 'development',
-  entry: './src/index.tsx',
+  mode: (process.env.NODE_ENV as "production" | "development" | undefined) ?? "development",
+  entry: "./src/index.tsx",
   module: {
     rules: [
       {
         test: /.tsx?$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
       },
     ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: [".tsx", ".ts", ".js"],
   },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'public', 'index.html'),
+      template: path.join(__dirname, "public", "index.html"),
     }),
   ],
   devServer: {
